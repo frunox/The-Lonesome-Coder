83ad2c3a0850f28f6e34276e7b5a73ec79fb7892
frunox <60527588+frunox@users.noreply.github.com>
2021-03-31 22:12:41 -0500

    switch to drop-down links for mobile

--- src/components/HomeNav/index.js
+++ src/components/HomeNav/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import DrawerToggleButton from '../SideDrawer/ToggleButton'
 import { useAuth } from "../../contexts/AuthContext"
 import './HomeNav.css'
 
@@ -9,25 +8,23 @@ const HomeNav = props => {
   const { currentUser } = useAuth()
 
   return (
-    <header className='toolbar'>
-      <nav className='toolbar_navigation'>
-        <div className='toolbar__toggle-button'>
-          <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-        <div className='toolbar_logo'><a href='/'>LOGO</a></div>
-        <div className='spacer' />
-        <div className='toolbar_nav-items'>
-          <ul>
-            {currentUser && <li><Link to="/profile">Profile</Link></li>}
-            {!currentUser &&
-              <>
-                <li><Link to='/login'>Log In</Link></li>
-                <li><Link to='/signup'>Sign Up</Link></li>
-              </>
-            }
-          </ul>
-        </div>
+    <header className='homenav-header'>
+      <div className='homenav-logo'><a href='/'>LOGO</a></div>
+      <input type='checkbox' id="navbar-toggle" className="navbar-toggle"></input>
+      <nav className='navbar-nav'>
+        <ul className='navbar-ul'>
+          {currentUser && <li><Link to="/profile">Profile</Link></li>}
+          {!currentUser &&
+            <>
+              <li><Link to='/login'>Log In</Link></li>
+              <li><Link to='/signup'>Sign Up</Link></li>
+            </>
+          }
+        </ul>
       </nav>
+      <label for='navbar-toggle' className='navbar-toggle-label'>
+        <span></span>
+      </label>
     </header>
   )
 }
