6383389f89276a58456e61b4715e2a81276ee1f1
frunox <60527588+frunox@users.noreply.github.com>
2021-04-08 13:21:47 -0500

    add file select and save to Firestore

--- src/components/auth/Admin/index.js
+++ src/components/auth/Admin/index.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import app from '../../../firebase'
 import './Admin.css'
 
 function Admin() {
+  const [file, setFile] = useState(null)
+  const [fileUrl, setFileUrl] = useState(null)
+
+  const fileSelectedHandler = event => {
+    // console.log(event.target.files[0])
+    setFile(event.target.files[0])
+  }
+
+  const parseHandler = () => {
+    console.log('Parse file', file.name)
+  }
+
+  const fileUploadHandler = async () => {
+    console.log('File selected: ', file)
+    let bucketName = 'markdown'
+    let selectedFile = file
+    console.log('selectedFile ', selectedFile)
+    let storageRef = app.storage().ref(`${bucketName}/${selectedFile.name}`)
+    await storageRef.put(file)
+    // get the URL for the file stored
+    const mdFileUrl = await storageRef.getDownloadURL()
+    console.log('File URL: ', mdFileUrl)
+    setFileUrl(mdFileUrl)
+  }
+
   return (
-    <div className='admin-content'>
-      <h2>Admin Page</h2>
-    </div>
+    <>
+      <div className='admin-content'>
+        <h2>Admin Page</h2>
+        <div className="admin-parse">
+          <h4>Choose file to parse:</h4>
+          <input type='file' onChange={fileSelectedHandler} />
+          <button onClick={parseHandler}>Parse Metadata</button>
+        </div>
+
+        <div className="admin-store">
+          <p>Store file</p>
+          <button onClick={fileUploadHandler}>Upload</button>
+        </div>
+      </div>
+
+    </>
   )
 }
 
