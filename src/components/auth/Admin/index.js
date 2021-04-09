0bd07dcea26fd5df8713fe5aca3cba3c32dd9c9b
frunox <60527588+frunox@users.noreply.github.com>
2021-04-09 12:07:52 -0500

    revised file processing, isolated content

--- src/components/auth/Admin/index.js
+++ src/components/auth/Admin/index.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import app from '../../../firebase'
+// import ReactMarkdown from 'react-markdown'
+// import gfm from 'remark-gfm'
+import Markdown from 'markdown-to-jsx'
 import './Admin.css'
 const parseMD = require("parse-md").default
 
@@ -7,7 +10,8 @@ const parseMD = require("parse-md").default
 function Admin() {
   const [file, setFile] = useState(null)
   const [mData, setMData] = useState('')
-  const [markdown, setMarkdown] = useState('')
+  const [markdownFile, setMarkdownFile] = useState('')
+  const [post, setPost] = useState('')
 
   const metadataFileSelectedHandler = async (event) => {
     // console.log(event.target.files[0])
@@ -19,7 +23,7 @@ function Admin() {
     reader.onload = await function () {
       let text = reader.result
       // console.log('text: ', text)
-      setMarkdown(text)
+      setMarkdownFile(text)
     };
     reader.onerror = await function () {
       console.log(reader.error);
@@ -27,26 +31,46 @@ function Admin() {
   }
 
   const parseHandler = async () => {
-    // console.log('markdown', markdown)
+    console.log("FILE: ", file.name)
+    // console.log('markdown file', typeof markdownFile, markdownFile)
 
-    const { metadata, content } = parseMD(markdown)
+    // usee parse-md to capture the metadata in the 'metadata' variable
+    const { metadata, content } = parseMD(markdownFile)
     setMData(metadata)
+    // setFile(content)
     // console.log('metadata: ', metadata)
 
     // remove metadata from .md file
-
+    // split the post file into an array of lines
+    const linesArray = markdownFile.split('\n')
+    linesArray.splice(0, 10)
+    console.log(linesArray)
+    let contentString = linesArray.join("\n")
+    console.log(contentString)
+    setPost(contentString)
+    // function to get the indices of the lines with "---"
+    // const getIndices = (acc, line, i) => {
+    //   if (/^---/.test(line)) {
+    //     acc.push(i)
+    //   }
+    //   return acc
+    // }
+    // use the function in reduce() to get an array with the indices
+    // const metadataIndices = linesArray.reduce(getIndices, [])
+    // console.log(metadataIndices)
+    // console.log('content', typeof content, content)
 
   }
 
-  const contentFileSelectedHandler = async (event) => {
-    // console.log(event.target.files[0])
-    setFile(event.target.files[0])
-  }
+  // const contentFileSelectedHandler = async (event) => {
+  //   // console.log(event.target.files[0])
+  //   setFile(event.target.files[0])
+  // }
 
   const fileUploadHandler = async (event) => {
     let bucketName = 'markdown'
     let selectedFile = file
-    console.log('selectedFile ', selectedFile)
+    // console.log('selectedFile ', selectedFile)
     let storageRef = app.storage().ref(`${bucketName}/${selectedFile.name}`)
     await storageRef.put(file)
     // get the URL for the file stored
@@ -74,17 +98,19 @@ function Admin() {
         <div className="admin-parse">
           <h4>Choose Metadata File to Parse:</h4>
           <input type='file' onChange={metadataFileSelectedHandler} />
-          <button onClick={parseHandler}>Parse Metadata</button>
+          <button onClick={parseHandler}>Parse Metadata and Preview File</button>
         </div>
 
         <div className="admin-store">
-          <h4>Choose Content File Upload to Storage:</h4>
-          <input type='file' onChange={contentFileSelectedHandler} />
+          <h4>Upload File to Cloud Storage:</h4>
           <button onClick={fileUploadHandler}>Upload File</button>
         </div>
         <div className="admin-metadata">
-          <p>Store Metadata to FireStore</p>
+          <h4>Store Metadata to FireStore</h4>
           <button onClick={metadataStoreHandler}>Store Metadata</button>
+        </div>
+        <div>
+          <Markdown>{post}</Markdown>
         </div>
       </div>
 
