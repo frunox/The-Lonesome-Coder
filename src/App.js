e1843ee43956d7eb6b6a72ed5293ce6a94806fae
frunox <jrcannon@msn.com>
2021-04-25 22:48:31 -0500

    remove auth related files and code

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { firestore } from './firebase';
 import { usePostsUpdate } from './contexts/PostContext';
-import PrivateRoute from './components/auth/PrivateRoute';
 import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ProfilePage from './pages/ProfilePage';
-import ForgotPassword from './components/auth/ForgotPassword/ForgotPassword';
-import UpdateProfile from './components/auth/UpdateProfile/UpdateProfile';
-import AdminPage from './pages/AdminPage';
 import AllPostsPage from './pages/AllPostsPage';
 import AboutPage from './pages/AboutPage';
 import Post from './components/Post';
-import NotFoundPage from './pages/NotFoundPage';
 import './App.css';
 
 function App() {
@@ -44,15 +36,8 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={AboutPage} />
-            <PrivateRoute path="/profile" component={ProfilePage} />
-            <PrivateRoute path="/update-profile" component={UpdateProfile} />
-            <PrivateRoute path="/admin" component={AdminPage} />
-            <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/signup" component={SignupPage} />
-            <Route path="/forgot-password" component={ForgotPassword} />
             <Route path="/all-posts" component={AllPostsPage} />
             <Route path="/post/:id" render={(props) => <Post {...props} />} />
-            <Route path="/404" component={NotFoundPage} />
             <Route path="*" component={Home} />
           </Switch>
         </Router>
